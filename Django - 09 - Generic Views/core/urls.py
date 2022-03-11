bcca1d89d26d8fad82f1129238c52756325e1c6f
Adriano M. Martins <adrianomqsmts@gmail.com>
2022-03-11 16:25:09 -0300

    ListView

--- Django - 09 - Generic Views/core/urls.py
+++ Django - 09 - Generic Views/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="index")
+    path("<int:pk>", PostDetailView.as_view(), name="detail")
 ]
--- Django - 09 - Generic Views/core/views.py
+++ Django - 09 - Generic Views/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView 
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from core.models import PostModel
 
@@ -8,3 +9,8 @@ from core.models import PostModel
 class PostListView(ListView):
     model = PostModel
     template_name = "index.html"
+
+class ModelDetailView(DetailView):
+    model = PostModel
+    context_object_name = 'post'
+    template_name = "details.html"
